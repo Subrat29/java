f3d793dd234ec94e2abcdab2c6194e889f7bf459
Subrat29 <ankubanku29062003@gmail.com>
2023-08-05 12:04:02 +0530

    4-8-23

---
+++ 16. Applet/First.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class First extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Hello", 150, 150);
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ 16. Applet/Second.java
@@ -0,0 +1,4 @@
+// <applet code=”demo” width=300 height=100>
+// <param name=fontName value=courier>
+// <param name=fontSize value=20>
+// </applet>
