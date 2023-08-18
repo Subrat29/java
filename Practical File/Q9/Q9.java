80dd6f38602167cb3af8494b57e18f7c1dc1d4c0
Subrat29 <ankubanku29062003@gmail.com>
2023-08-18 22:10:39 +0530

    done babes 18-8

--- Practical File/Q9/Q9.java
+++ Practical File/Q9/Q9.java
@@ -1,17 +1,30 @@
-class AgeException {
+//own exception class
+class AgeException extends Exception {
+    public String toString() {
+        return "Above age exception";
+    }
 }
 
 class Employee extends AgeException {
-
     String name;
     int age;
 
     public static void main(String[] args) {
+
         Employee e = new Employee();
         e.name = args[0];
         e.age = Integer.parseInt(args[1]);
 
-        System.out.println("Name : " + e.name);
-        System.out.println("Age : " + e.age);
+        try {
+            if (e.age > 60) {
+                throw new AgeException();
+            }
+
+        } catch (AgeException ae) {
+            System.out.println(ae);
+        } finally {
+            System.out.println("Name : " + e.name);
+            System.out.println("Age : " + e.age);
+        }
     }
 }
