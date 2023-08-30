10af790fac962489f65960ea8ad2837a35583df6
Subrat29 <ankubanku29062003@gmail.com>
2023-08-30 11:40:59 +0530

    30-8

---
+++ Practical File/Q19/Q19.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Q18 implements KeyListener {
+
+    JFrame frame;
+    JTextField textfield;
+    
+
+    
+
+    public void keyPressed(KeyEvent k) {
+
+    }
+
+    public void keyReleased(KeyEvent k) {
+
+    }
+
+    public void keyTyped(KeyEvent k) {
+
+    }
+
+}
+
+public static void main(String[] args) {
+    new Q18();
+}
